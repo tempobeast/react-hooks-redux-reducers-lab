9000a9a1843565d7530a92a807aa2e49bbe9a850
Zach <shaw.zacht@gmail.com>
2022-08-10 11:24:24 -0700

    commit tree 1f89fe7fae6c7c9850eb4f7e4fc32a7c8c7bd0cd

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,10 @@
+// const initialState = { friends: [] };
+
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {friends:[...state.friends, action.friend]};
+    default:
+      return state;
+  }
 }
